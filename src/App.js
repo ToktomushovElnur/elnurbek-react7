79d02bc65d3c1ac282cc1f9a50e03bce3aedd74a
ToktomushovElnur <Elnurbek2004mr@example.com>
2024-01-02 16:06:07 +0600

    Toktomishov Elnurbek

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// Buttons.js
+import React, { useMemo, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Buttons = () => {
+    const [counterOne, setCounterOne] = useState(0);
+    const [counterTwo, setCounterTwo] = useState(0);
 
-export default App;
+    const handleClickOne = useMemo(() => {
+        return () => {
+            setCounterOne(counterOne + 1);
+            console.log(`Button One Clicked! Counter One: ${counterOne}`);
+        };
+    }, [counterOne]); // Мемоизация функции для кнопки 1
+
+    const handleClickTwo = useMemo(() => {
+        return () => {
+            setCounterTwo(counterTwo + 1);
+            console.log(`Button Two Clicked! Counter Two: ${counterTwo}`);
+        };
+    }, [counterTwo]); // Мемоизация функции для кнопки 2
+
+    return (
+        <div>
+            <p>Counter One: {counterOne}</p>
+            <button onClick={handleClickOne}>Increment Counter One</button>
+            <p>Counter Two: {counterTwo}</p>
+            <button onClick={handleClickTwo}>Increment Counter Two</button>
+        </div>
+    );
+};
+
+export default Buttons;
